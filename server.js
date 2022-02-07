730afee43b443e540e1a1890a9bff6d0fc80b183
JoseapNeto <netimsccp@gmail.com>
2022-02-07 05:14:19 -0300

    Publicação Heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/CadastroCliente'));
+app.use(express.static('./dist/clientes-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/CadastroCliente/'}),
+    res.sendFile('index.html', {root: 'dist/clientes-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
